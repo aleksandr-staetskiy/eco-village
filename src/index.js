a5141ec9198d56027d8f6287fc2896bc3c1951d0
Aleksandr Staetsky <myexploretime@gmail.com>
2019-05-31 20:30:04 +0300

    Init empty branch

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// JS - ./js/index.js
+import './js/'
+
+// SCSS
+import './scss/main.scss'
+
+// CSS (example)
+import './css/main.css'
+
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', { autoplay: 2000 }).mount()
+
+// Bootstrap (example)
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
