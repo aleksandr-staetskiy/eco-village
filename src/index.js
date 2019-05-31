231b5316ee1b0a643f3475095c6a3697b48ffe85
Aleksandr Staetsky <myexploretime@gmail.com>
2019-05-31 20:26:54 +0300

    home page layout

--- src/index.js
+++ src/index.js
@@ -7,24 +7,11 @@ import './scss/main.scss'
 // CSS (example)
 import './css/main.css'
 
+import Glide from '@glidejs/glide'
+
+new Glide('.glide', { autoplay: 2000 }).mount()
+
 // Bootstrap (example)
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-// Vue.js
-window.Vue = require('vue')
-import store from './store'
-
-// Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)
-
-// Vue init
-const app = new Vue({
-  data () {
-    return {
-      showExampleComponent: false,
-    }
-  },
-  store,
-  el: '#app'
-})
